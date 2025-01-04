6e4d498de4290a2e208c32b8a26a3df27107b865
Kiran ram <kram@lions.molloy.edu>
2025-01-03 21:40:45 -0500

    added get task id endpoint and created tests for GET

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify, abort
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 import os # gives access to operating system 
@@ -7,6 +7,7 @@ from alembic import op
 from sqlalchemy.sql import text # allows the use of raw SQL using text()
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine, Column, Integer, String
+
 
 load_dotenv() #loading environment files. Environment configuration files. These you can have access anywhere in the app. 
 
@@ -25,15 +26,6 @@ class Task(db.Model):
     completed = db.Column(db.Boolean, default=False)
 
 
-
-@app.route('/test-db')
-def test_db():
-    try:
-        db.session.execute('SELECT 1')  # A simple query to test connection
-        return {"message": "Database connection successful"}
-    except Exception as e:
-        return {"message": str(e)}
-
 @app.route("/kiranvalue")
 def home():
     return {'kiran_value_from_env_file': os.getenv('KIRAN')}
@@ -42,17 +34,41 @@ def home():
 @app.route('/tasks', methods=['POST']) #flask decorator instead of using URL pattern
 def create_tasks():
     task1 = Task(title="HW", completed = True) #A Task instance (task1) is created with the title "HW" and completed=True.
-    
-
-    
     db.session.add(task1)  #The task1 instance is added to the SQLAlchemy session (db.session.add(task1)).
     db.session.commit() #The session changes are committed to the database (db.session.commit()), which executes the SQL INSERT statement.
     db.session.refresh(task1) # Ensure task1 remains bound to the session
-
     return {'task1_title': task1.title}
+
+@app.route('/tasks',methods=['GET'])
+def get_tasks():
+    tasks = Task.query.all()
+    tasks_list = []
+    for task in tasks:
+        task_dict = {
+            'id': task.id,
+            'title' : task.title,
+            'completed' : task.completed
+        }
+        tasks_list.append(task_dict)
+    return jsonify({'tasks' : tasks_list}), 200 #proving response code as second return value. It is good practice to wrap return values in jsonify as it fixes format
+
+@app.route('/tasks/<int:id>', methods=['GET'])
+def get_task_id(id):
+    task = Task.query.get(id) # query.get is outdated in SQLAlchemy so use session
+    if not task:
+        abort(404, description="Task not Found")
+    return jsonify({
+            "id" : task.id,
+            "title" : task.title,
+            "completed" : task.completed
+    }), 200
+
+
 
 if __name__ == "__main__":     
     app.run(debug=True)   
+    
+
 
 
 
